435aeec2621db222348cf718531dbf7988e1b4fb
Jihyun_Kim <jihyun3313@daum.net>
2023-04-29 19:01:46 +0900

    메인화면 레지스터섹션 작업중

--- app/(main)/(Register)/index.tsx
+++ app/(main)/(Register)/index.tsx
@@ -1,9 +1,96 @@
 import styles from './styles.module.scss';
+import Typo from '@components/core/Typo';
+import Paw from '@assets/svg/paw.svg';
+import dog1 from '@assets/png/main/dog1.png';
+import dog2 from '@assets/png/main/dog2.png';
 
-export default function Register () {
-  return(
-    <div className={styles.registerSectionLayout}>
-      아아아아
+const DogList: Dog[] = [
+  {
+    img: dog1.src,
+    name: "코코",
+    reported: true,
+    age: 4,
+    sex: "남아",
+    neutered: true,
+    breed: "웰시코기",
+    memo: " 메모  22년 10월에 건강검진 완료",
+    disease: "견과류 알레르기"
+  },
+  {
+    img: dog2.src,
+    name: "코쿤",
+    reported: false,
+    age: 1,
+    sex: "여아",
+    neutered: false,
+    breed: "웰시코기",
+    memo: " 메모  22년 10월에 건강검진 완료",
+    disease: ""
+  }
+]
+
+interface Dog {
+  img: string;
+  name: string;
+  reported: boolean;
+  age: number;
+  sex: string;
+  neutered: boolean;
+  breed: string;
+  memo: string;
+  disease: string;
+}
+
+interface DogCardProps {
+  dog: Dog;
+}
+
+const DogCard = (props: DogCardProps) => {
+  const { dog } = props;
+
+  return (
+    <div className={styles.dogCard}>
+      <div className={styles.dogImg} style={{ backgroundImage: `url(${dog.img})` }} />
+
+
+    </div>
+
+  )
+}
+
+
+
+export default function Register() {
+
+
+  return (
+    <div className={styles.sectionLayout}>
+
+      <div className={styles.titleSection}>
+        <Paw viewBox="0 0 24 24" className={styles.icon} />
+        <div className={styles.titleWrapper}>
+          <Typo variant="t3" color="black" style={{ display: 'inline' }} className={styles.title}>
+            <>
+              내{' '}
+              <Typo variant="t3" color="#0074DD" style={{ display: 'inline' }}>
+                반려견
+              </Typo>
+              을 등록할게요
+            </>
+          </Typo>
+        </div>
+      </div>
+
+      <div className={styles.dogList}>
+        {DogList.map((el: Dog) => {
+          return <DogCard dog={el} key={el.name} />
+        })}
+
+
+      </div>
+
+
+
     </div>
   )
 }
