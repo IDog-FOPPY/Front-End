4bdb988c2503e243ba959ddec5470b0a10cf73e9
JIWON_MIN <mjw3534@gmail.com>
2023-04-29 19:15:21 +0900

    메인 화면 초기 작업

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,13 @@
 import HeroBanner from "./(HeroBanner)";
+import Lost from "./(Lost)";
 import Register from "./(Register)";
 
 export default function MainPage() {
   return (
     <>
       <HeroBanner />
-      <Register />
+      {/* <Register /> */}
+      <Lost />
     </>
   )
 }
