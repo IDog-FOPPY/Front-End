3ef235a10a02b4c238cb309557acba55f7935f78
JIWON_MIN <mjw3534@gmail.com>
2023-04-29 16:31:11 +0900

    메인 화면 초기 작업

---
+++ app/(main)/(Register)/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export default function Register () {
+  return(
+    <div className={styles.registerSectionLayout}>
+      아아아아
+    </div>
+  )
+}
--- src/components/core/Typo/index.tsx
+++ src/components/core/Typo/index.tsx
@@ -1,17 +1,39 @@
+"use client";
+
+import { useDevice } from "src/logics/hooks/useDevice";
+
 // TODO ::: 의논 및 수정 필요
-const VariantMapping = {
-  'h1' : '32px',
-  'h2' : '24px',
-  'h3' : '19px',
-  'h5' : '13px',
-  'h6' : '11px',
-  // t1 : 
-  // t2 :
-  // caption : 
+const DesktopMapping = {
+  'h1' : {fontSize: '80px', linHeight: '96px'},
+  'h2' : {fontSize: '50px', linHeight: '70px'},
+  'h3' : {fontSize: '40px', linHeight: '50px'},
+  'h4' : {fontSize: '36px', linHeight: '60px'},
+  'h5' : {fontSize: '34px', linHeight: '50px'},
+  'h6' : {fontSize: '32px', linHeight: '50px'},
+  't1' : {fontSize: '26px', linHeight: '40px'},
+  't2' : {fontSize: '24px', linHeight: '34px'},
+  't3' : {fontSize: '22px', linHeight: '32px'},
+  'caption' : {fontSize: '20px', linHeight: '24px'},
+  // 'footnote' : '14px',
 }
+
+const MobileMapping = {
+  'h1' : {fontSize: '40px', linHeight: '48px'},
+  'h2' : {fontSize: '30px', linHeight: '42px'},
+  'h3' : {fontSize: '26px', linHeight: '36px'},
+  'h4' : {fontSize: '24px', linHeight: '34px'},
+  'h5' : {fontSize: '22px', linHeight: '32p'},
+  'h6' : {fontSize: '20px', linHeight: '32px'},
+  't1' : {fontSize: '18px', linHeight: '28px'},
+  't2' : {fontSize: '16px', linHeight: '26px'},
+  't3' : {fontSize: '14px', linHeight: '22px'},
+  'caption' : {fontSize: '8px', linHeight: '10px'},
+  // 'footnote' : '6px',
+}
+
 interface TypoProps {
   bold?: boolean;
-  variant: 'h1' | 'h2' | 'h3' | 'h5' | 'h6'; 
+  variant: 'h1' | 'h2' | 'h3' | 'h5' | 'h6' | 't1' | 't2' | 't3' | 'caption'; 
   color: string;
   style?: object;
   className?: string;
@@ -19,12 +41,20 @@ interface TypoProps {
 }
 
 export default function Typo (props : TypoProps) {
-  const { bold=false, variant='h2', color, style, className, children } = props;
+  const { bold = false, variant='h2', color, style, className, children } = props;
+  const device = useDevice();
+
+  const renderFontSize = () => {
+    if(device === 'mobile') return MobileMapping[variant];
+    else return DesktopMapping[variant];
+  }
+
   return (
     <div 
     style={{
       fontWeight: bold ? 700 : 400, 
-      fontSize: VariantMapping[variant], 
+      fontSize: renderFontSize().fontSize,
+      lineHeight: renderFontSize().linHeight,
       color: color, 
       ...style
     }} 
