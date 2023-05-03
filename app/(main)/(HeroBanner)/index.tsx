bcec4e571916a3df49638dee366c94be8297d1ca
Jihyun_Kim <jihyun3313@daum.net>
2023-05-03 14:43:29 +0900

    메인화면 레지스터섹션 완료

--- app/(main)/(HeroBanner)/index.tsx
+++ app/(main)/(HeroBanner)/index.tsx
@@ -6,19 +6,19 @@ import { useDevice } from 'src/logics/hooks/useDevice';
 // import ArrowRight from '@assets/svg/arrow-right-long.svg';
 import styles from './styles.module.scss';
 
-export default function HeroBanner () {
+export default function HeroBanner() {
   const device = useDevice();
   const reported = 0;
-  const finded = 0; 
+  const finded = 0;
 
   return (
-    <div className={styles.heroBannerContainer} style={{backgroundImage: `url(${bgImg.src})`}}>
+    <div className={styles.heroBannerContainer} style={{ backgroundImage: `url(${bgImg.src})` }}>
       <div>
         <Typo variant={device === 'mobile' ? "h6" : "h2"} bold color="white">
           <>
-            소중한 나의 반려견, 
-            <br/> 
-            <Typo variant={device === 'mobile' ? "h6" : "h2"} bold color="#0074DD" style={{display: 'inline'}}>
+            소중한 나의 반려견,
+            <br />
+            <Typo variant={device === 'mobile' ? "h6" : "h2"} bold color="#0074DD" style={{ display: 'inline' }}>
               비문
             </Typo>
             등록부터 함께해요.
@@ -34,7 +34,7 @@ export default function HeroBanner () {
         <Typo variant='t3' color="#5B5B5B">
           <>
             오늘 신고된 강아지{' '}
-            <Typo variant="t2" bold color="#0074DD" style={{display: 'inline'}}>
+            <Typo variant="t2" bold color="#0074DD" style={{ display: 'inline' }}>
               {reported.toString()}
             </Typo>
             마리
@@ -43,7 +43,7 @@ export default function HeroBanner () {
         <Typo variant='t3' color="#5B5B5B">
           <>
             오늘 찾은 강아지{' '}
-            <Typo variant="t2" bold color="#0074DD" style={{display: 'inline'}}>
+            <Typo variant="t2" bold color="#0074DD" style={{ display: 'inline' }}>
               {finded.toString()}
             </Typo>
             마리
--- app/(main)/(Register)/index.tsx
+++ app/(main)/(Register)/index.tsx
@@ -5,28 +5,8 @@ import dog1 from '@assets/png/main/dog1.png';
 import dog2 from '@assets/png/main/dog2.png';
 
 const DogList: Dog[] = [
-  {
-    img: dog1.src,
-    name: "코코",
-    reported: true,
-    age: 4,
-    sex: "남아",
-    neutered: true,
-    breed: "웰시코기",
-    memo: " 메모  22년 10월에 건강검진 완료",
-    disease: "견과류 알레르기"
-  },
-  {
-    img: dog2.src,
-    name: "코쿤",
-    reported: false,
-    age: 1,
-    sex: "여아",
-    neutered: false,
-    breed: "웰시코기",
-    memo: " 메모  22년 10월에 건강검진 완료",
-    disease: ""
-  }
+  { img: dog1.src, name: "코코", reported: true, age: 4, sex: "남아", neutered: true, breed: "웰시코기", memo: " 메모  22년 10월에 건강검진 완료", disease: "견과류 알레르기" },
+  { img: dog2.src, name: "코쿤", reported: false, age: 1, sex: "여아", neutered: false, breed: "웰시코기", memo: " 메모  22년 10월에 건강검진 완료", disease: "" }
 ]
 
 interface Dog {
@@ -48,13 +28,110 @@ interface DogCardProps {
 const DogCard = (props: DogCardProps) => {
   const { dog } = props;
 
+  const renderReported = () => {
+    if (dog.reported === true)
+      return (
+        <div className={styles.reported}>
+          <Typo variant="footnote" color="white">
+            실종신고
+          </Typo>
+        </div>
+      )
+    else return;
+  }
+
+  const renderNeutered = () => {
+    if (dog.neutered === true) return (
+      <Typo variant="footnote" color="#0074DD">
+        중성화 O
+      </Typo>
+    )
+    else return (
+      <Typo variant="footnote" color="#0074DD">
+        중성화 X
+      </Typo>
+    )
+  }
+
   return (
     <div className={styles.dogCard}>
       <div className={styles.dogImg} style={{ backgroundImage: `url(${dog.img})` }} />
 
+      <div className={styles.dogInfo}>
 
+        <div className={styles.headerSection}>
+          <Typo variant="t1" bold color="black">
+            <>
+              {dog.name}
+            </>
+          </Typo>
+          {renderReported()}
+        </div>
+
+        <div className={styles.contentSection}>
+          <div className={styles.contentLeft}>
+            <div className={styles.contentEl}>
+              <Typo variant="footnote" color="#0074DD" >
+                나이
+              </Typo>
+              <Typo variant="footnote" color="black" className={styles.content}>
+                <>
+                  {dog.age.toString()}세
+                </>
+              </Typo>
+            </div>
+            <div className={styles.contentEl}>
+              <Typo variant="footnote" color="#0074DD">
+                성별
+              </Typo>
+              <Typo variant="footnote" color="black" className={styles.content}>
+                <>
+                  {dog.sex}
+                </>
+              </Typo>
+              <div className={styles.neutered}>
+                {renderNeutered()}
+              </div>
+
+            </div>
+            <div className={styles.contentEl}>
+              <Typo variant="footnote" color="#0074DD">
+                견종
+              </Typo>
+              <Typo variant="footnote" color="black" className={styles.content}>
+                <>
+                  {dog.breed}
+                </>
+              </Typo>
+            </div>
+          </div>
+
+
+          <div className={styles.contentRight}>
+            <div className={styles.contentEl}>
+              <Typo variant="footnote" color="#0074DD" >
+                메모
+              </Typo>
+              <Typo variant="footnote" color="black" className={styles.content} style={{ whiteSpace: 'normal' }}>
+                <>
+                  {dog.memo}
+                </>
+              </Typo>
+            </div>
+            <div className={styles.contentEl}>
+              <Typo variant="footnote" color="#0074DD">
+                질병
+              </Typo>
+              <Typo variant="footnote" color="black" className={styles.content} style={{ whiteSpace: 'normal' }}>
+                <>
+                  {dog.disease}
+                </>
+              </Typo>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
   )
 }
 
@@ -85,10 +162,13 @@ export default function Register() {
         {DogList.map((el: Dog) => {
           return <DogCard dog={el} key={el.name} />
         })}
-
-
       </div>
 
+      <div className={styles.addDog}>
+        <Typo variant="h2" color="#9F9F9F">
+          +
+        </Typo>
+      </div>
 
 
     </div>
