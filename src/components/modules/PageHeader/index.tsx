119226a583bac7f438a6d6599e9459539af586eb
JIWON_MIN <mjw3534@gmail.com>
2023-05-16 15:49:36 +0900

    drawer 작업 중

--- src/components/modules/PageHeader/index.tsx
+++ src/components/modules/PageHeader/index.tsx
@@ -1,10 +1,19 @@
+"use client"
+
+import { useState } from 'react';
+import DrawerIcon from '@assets/svg/drawer.svg';
 import styles from './styles.module.scss';
 
 // 현재 모바일 화면 기준
 export default function PageHeader() {
+  const [open, setOpen] = useState(false);
+
   return(
+    !open ?
     <div className={styles.headerContainer}>
-      {/* 이후 drawer 삽입 예정 */}
+      <DrawerIcon className={styles.drawerIcon} viewBox="0 0 20 14" width="24px" height="24px" />
     </div>
+    : <div className={styles.drawer}>
+      </div>
   )
 }
