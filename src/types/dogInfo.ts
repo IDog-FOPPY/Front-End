f2284a591c233038b270d22a09876d68d1628ca1
JIWON_MIN <mjw3534@gmail.com>
2023-06-17 16:53:47 +0900

    doginfo 수정

--- src/types/dogInfo.ts
+++ src/types/dogInfo.ts
@@ -1,14 +1,27 @@
 import { StaticImageData } from "next/image";
 
 export interface DogInfo {
-  id?: number;
-  img?: StaticImageData;
-  name?: string;
-  reported?: boolean;
-  age?: number;
-  sex?: string;
-  neutered?: boolean;
-  breed?: string;
-  memo?: string;
+  // 변수명 수정 필요
+  id?: number; // petId
+  name?: string; // petName
+  sex?: boolean; // petSex (true 여자 false 남자)
+  breed?: string; // petBreed
+  age?: number; // petOld
   disease?: string;
+  neutered?: boolean;
+  memo?: string; // note
+
+  reported?: boolean; // missed
+  missCity?: string;
+  missGu?: string;
+  missDong?: string;
+  missDetail?: string;
+  missTime?: string;
+  missDate?: string;
+  etc?: string;
+
+  createdDate?: string;
+
+  // img?: StaticImageData; // 백 미구현 상태
+
 }
