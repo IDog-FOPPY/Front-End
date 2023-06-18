646dfafde1095b49bfab4db5843176e2ad8c2559
Jihyun_Kim <jihyun3313@daum.net>
2023-06-18 18:45:00 +0900

    비문일치여부 페이지 구현
    개 카드 출력은 api 없어서 미구현

--- app/noseid-match-fail/page.tsx
+++ app/noseid-match-fail/page.tsx
@@ -1,9 +1,11 @@
 // 비문 탐색 결과 페이지 - 실패
 
+import Link from 'next/link'
 import styles from './styles.module.scss';
 import Typo from '@components/core/Typo';
 import logo from '@assets/Logo.png';
-import Sadface from '@assets/svg/noseMatch/sadface.svg'
+import Sadface from '@assets/svg/noseMatch/sadface.svg';
+import Arrow from '@assets/svg/noseMatch/arrow.svg';
 
 export default function NoseIdMatchFailPage() {
 
@@ -16,9 +18,13 @@ export default function NoseIdMatchFailPage() {
         <Sadface style={{ marginLeft: "3px" }} />
       </Typo>
 
-      <Typo variant="t2" color="#606060" className={styles.subTitle}>
-        유기견 게시판 가기
-      </Typo>
+      <Link href={{ pathname: '/lost-dog-list' }}>
+        <Typo variant="t2" color="#606060" className={styles.subTitle}>
+          유기견 게시판 가기
+        </Typo>
+        <Arrow />
+      </Link>
+
 
 
     </>
