6d83d4f01a902d82c82277b55129b1ea4f2f44be
Jihyun_Kim <jihyun3313@daum.net>
2023-08-21 00:00:26 +0900

    // WIP

--- app/chatting-list/page.tsx
+++ app/chatting-list/page.tsx
@@ -5,10 +5,64 @@ import { getChattingList } from "@src/logics/axios";
 import styles from "./styles.module.scss";
 import ArrowLeft from '@assets/svg/register/arrow-left.svg';
 import Typo from '@components/core/Typo';
+import dayjs from "dayjs";
+
+let uid = parseInt(localStorage.getItem('foppy_user_uid') || '{}');
+
+interface Chatting {
+  id: number;
+  member1Id: number;
+  member2Id: number;
+  member1NickName: string;
+  member2NickName: string;
+  member1ProfileImgUrl: string;
+  member2ProfileImgUrl: string
+
+  lastMessageCreatedAt: string;
+  lastMessage: string;
+
+}
+
+interface ChattingListProps {
+  chatting: Chatting;
+}
+
+
+const ChattingList = (props: ChattingListProps) => {
+  const { chatting } = props;
+  const d = dayjs(chatting.lastMessageCreatedAt);
+  const lastDate = d.format("YY/MM/DD");
+
+  // 상대방 닉네임 get
+  const matchUser = (id: number) => {
+    if (id === uid) {
+      return chatting.member2NickName;
+    }
+    else {
+      return chatting.member1NickName;
+    }
+  }
+  const partnerName = matchUser(chatting.member1Id);
+
+  return (
+
+    <div className={styles.chattingList}>
+      <img alt="dog-image" src={chatting.member2ProfileImgUrl} className={styles.profileImg} />
+      {partnerName}
+      {lastDate}
+      {chatting.lastMessage}
+    </div>
+
+
+  )
+
+
+
+}
+
+
 export default function ChattingListPage() {
-
   const [chattings, setChattings] = useState([]);
-
   useEffect(() => {
     const getData = async () => {
       setChattings(await getChattingList());
@@ -16,7 +70,9 @@ export default function ChattingListPage() {
 
     const accessToken = localStorage.getItem("foppy_auth_token");
     if (accessToken) getData();
+    console.log('채팅목록', chattings);
   }, []);
+  console.log(localStorage.getItem('foppy_user_uid'));
 
 
   return (
@@ -28,7 +84,15 @@ export default function ChattingListPage() {
           <div className={styles.blank}></div>
         </div>
       </div>
-//chattingList
+
+      <div>
+
+        {chattings.map((chat: Chatting) => {
+          return <ChattingList chatting={chat} key={chat.id} />;
+        })}
+
+      </div>
+
 
     </>
   );
