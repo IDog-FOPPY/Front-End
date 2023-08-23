0cd8d0757508aef9c77dc1d290ed7ad03c3f0e9e
MINJIWON <z_zzz7@fount.co>
2023-08-23 23:23:37 +0900

    chatting/page.tsx 수정

--- app/chatting/page.tsx
+++ app/chatting/page.tsx
@@ -1,78 +1,31 @@
 "use client";
-import { useEffect, useState } from 'react';
 
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from "next/navigation";
 import { getChatting } from "@src/logics/axios";
 import { postNewChatting } from "@src/logics/axios";
-import Chatting from '@src/screens/chatting';
+import Chatting from "@src/screens/chatting";
 
-import Link from 'next/link'
-import styles from './styles.module.scss';
-import Typo from '@components/core/Typo';
-
-
-export default function ChattingPage() {
+export default async function ChattingPage() {
   const state = useSearchParams().get("state");
-  const id = parseInt(useSearchParams().get("id") || '{}');
-  const uid = parseInt(localStorage.getItem('foppy_user_uid') || '{}');
-  console.log("uid", uid);
+  const id = parseInt(useSearchParams().get("id") || "{}");
+  // token, uid -> swr 사용하는 방식으로 수정 예정
+  const uid = parseInt(localStorage.getItem("foppy_user_uid") || "{}");
   //const [chatting, setChatting] = useState([]);
 
-
-  if (state === "old") {
-    console.log("기존채팅방get 방ID는", id);
-    const chatting = async () => await getChatting(id);
-  }
-  else if (state === "new") {
-    console.log("새 채팅방 post 개 ID는", id);
-    if (uid && id) {
-      async () => await postNewChatting({
-        userId: uid,
-        dogId: id,
-      })
+  try {
+    if (state === "old") {
+      const chatting = await getChatting(id);
+    } else if (state === "new") {
+      if (uid && id) {
+        const chatting = await postNewChatting({
+          userId: uid,
+          dogId: id,
+        });
+      }
     }
+  } catch (err) {
+    console.log(err);
   }
-  else console.log("err");
 
-
-
-
-  // useEffect(() => {
-  //   if (state === "old") {
-  //     console.log("기존채팅방get 방ID는", id);
-  //     const chatting = async () => await getChatting(id);
-  //   }
-  //   else if (state === "new") {
-  //     console.log("새 채팅방 post 개 ID는", id);
-  //     if (uid && id) {
-  //       async () => await postNewChatting({
-  //         userId: uid,
-  //         dogId: id,
-  //       })
-  //     }
-  //   }
-  //   else console.log("err");
-  // }, []);
-
-
-  // if (state === "old") {
-  //   console.log("기존채팅방get 방ID는", id);
-  //   useEffect(() => {
-  //     const getData = async () => {
-
-  //       setChatting(await getChatting(id));
-  //     };
-  //     getData();
-  //   }, []);
-  // }
-
-
-
-
-
-
-  return (
-    <Chatting />
-
-  )
+  return <Chatting />;
 }
