2ae455d1b9562998a93d9f5664d7c793e426e72a
Jihyun_Kim <jihyun3313@daum.net>
2023-08-24 20:36:57 +0900

    채팅방 수정
    + 엔드포인트수정
    + 토큰 & uid : 14번유저

--- app/chatting/page.tsx
+++ app/chatting/page.tsx
@@ -9,24 +9,50 @@ export default function ChattingPage() {
   const state = useSearchParams().get("state");
   const id = parseInt(useSearchParams().get("id") || "{}");
   // token, uid -> swr 사용하는 방식으로 수정 예정
-  const uid = 13;
+  const uid = 14;
   // const uid = parseInt(localStorage.getItem("foppy_user_uid") || "{}");
   //const [chatting, setChatting] = useState([]);
 
   console.log(state, id, uid);
 
+
+
+
+
+
   try {
-    if (state === "old") {
-      const chatting = async () => await getChatting(id);
-    } else if (state === "new") {
-      if (uid && id) {
-        const chatting = async () =>
-          await postNewChatting({
+    // if (state === "old") {
+    //   const chatting = async () => await getChatting(id);
+    // } else if (state === "new") {
+    //   if (uid && id) {
+    //     console.log("새채팅생성");
+    //     const chatting = async () =>
+    //       await postNewChatting({
+    //         userId: uid,
+    //         dogId: id,
+    //       });
+    //     console.log("res", chatting);
+    //   }
+    // }
+
+
+    const getData = async () => {
+      if (state === "old") {
+        const chatting = await getChatting(id);
+      } else if (state === "new") {
+        if (uid && id) {
+          console.log("새채팅생성");
+          const chatting = await postNewChatting({
             userId: uid,
             dogId: id,
           });
+          console.log("res", chatting);
+        }
       }
-    }
+    };
+    getData();
+
+
   } catch (err) {
     console.log(err);
   }
--- src/logics/axios.ts
+++ src/logics/axios.ts
@@ -1,14 +1,14 @@
 import Axios from "axios";
 
 export const axios = Axios.create({
-  baseURL: "http://3.38.247.212:8080/api",
+  baseURL: "http://13.125.180.85:8080/api",
   // timeout: 30000,
   headers: {
     // Authorization: "Bearer " + localStorage.getItem("foppy_auth_token"),
     // chat1
     // Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImNoYXQxIiwiaWF0IjoxNjkyNzE1MjQwLCJleHAiOjE2OTI3MTg4NDB9.4SJpIOpfU7dMUINpMm-Pi1bFTTdjuVctlQeVtAxga0Y"
     // chat2
-    Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImNoYXQxIiwiaWF0IjoxNjkyNzk1ODY0LCJleHAiOjE2OTI3OTk0NjR9.WBpUosK5VIqLCCUd0dsL9MAa8xX_h6olxBh5HM006Ro"
+    Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImNoYXQyIiwiaWF0IjoxNjkyODc1NzkwLCJleHAiOjE2OTI4NzkzOTB9.X0fyLMgDHna6RX0kaIFfLDysjsdDxCRLCJ_aKH_xRGo"
   },
 });
 
