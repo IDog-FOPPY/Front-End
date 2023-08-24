4d238e0a44c46250b4ea996bf6e9ce708282e09f
Jihyun_Kim <jihyun3313@daum.net>
2023-08-25 01:48:02 +0900

    // WIP

--- src/logics/axios.ts
+++ src/logics/axios.ts
@@ -8,7 +8,7 @@ export const axios = Axios.create({
     // chat1
     // Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImNoYXQxIiwiaWF0IjoxNjkyNzE1MjQwLCJleHAiOjE2OTI3MTg4NDB9.4SJpIOpfU7dMUINpMm-Pi1bFTTdjuVctlQeVtAxga0Y"
     // chat2
-    Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImNoYXQyIiwiaWF0IjoxNjkyODc1NzkwLCJleHAiOjE2OTI4NzkzOTB9.X0fyLMgDHna6RX0kaIFfLDysjsdDxCRLCJ_aKH_xRGo"
+    Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImppaHl1biIsImlhdCI6MTY5Mjg5MzU3NywiZXhwIjoxNjkyODk3MTc3fQ.5hiJFIeRJVedYBwZfSlFa22xDgYCYsU2QsZVzbH_QiU"
   },
 });
 
--- src/screens/chatting/index.tsx
+++ src/screens/chatting/index.tsx
@@ -1,10 +1,71 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
+import { Stomp, Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
 import styles from "./styles.module.scss";
 import Typo from "@components/core/Typo";
+//import * as SockJS from 'sockjs-client';
+
+const token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImppaHl1biIsImlhdCI6MTY5Mjg5MzU3NywiZXhwIjoxNjkyODk3MTc3fQ.5hiJFIeRJVedYBwZfSlFa22xDgYCYsU2QsZVzbH_QiU";
+const senderId = 17;  //jihyun
+const receiverId = 20;  //mongg
+//방아이디 19
 
 export default function ChattingPage() {
-  return <div></div>;
+  const [content, setContent] = useState("");
+
+
+  // var stompClient = null;
+  var socket = new SockJS('/ws/chat');
+  var stompClient = Stomp.over(socket);
+
+
+
+
+  stompClient.connect({ 'Authorization': token }, function (frame: any) {
+    console.log("frame", frame);
+    // stompClient.subscribe('/topic/messages', function (result) {
+    stompClient.subscribe('/sub/room/19', function (result: any) {
+
+      // show(JSON.parse(result.body));
+
+      console.log("res", JSON.parse(result.body))
+    });
+  });
+
+
+
+  // stompClient.connect({ 'Authorization': token }, function (frame: any) {
+  //   console.log("frame", frame);
+  //   // stompClient.subscribe('/topic/messages', function (result) {
+  //   stompClient.subscribe('/sub/room/19', function (result: any) {
+
+  //     // show(JSON.parse(result.body));
+
+  //     console.log("res", JSON.parse(result.body))
+  //   });
+  // });
+
+  function sendMessage() {
+
+    stompClient.send("/pub/send", {},
+      JSON.stringify({ 'content': content, 'senderId': senderId, 'receiverId': receiverId }));
+  }
+
+
+
+
+  return (
+    <>
+      <div>
+
+        <input type="text" id="content" placeholder="Text" onChange={(e) => setContent(e.target.value)} />
+        <button id="sendMessage" onClick={sendMessage}>Send</button>
+
+
+      </div>;
+    </>
+  )
+
 }
