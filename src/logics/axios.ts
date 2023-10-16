d41f26565ceb0aef76487cf87eee29d60ced3532
Jihyun_Kim <jihyun3313@daum.net>
2023-10-16 20:10:26 +0900

    엔드포인트 변경

--- src/logics/axios.ts
+++ src/logics/axios.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const axios = typeof window !== 'undefined' ? Axios.create({
-  baseURL: "http://54.180.156.211:8080/api",
+  baseURL: "http://3.36.63.57:8080/api",
   // timeout: 30000,
   headers: {
     Authorization: "Bearer " + localStorage.getItem("foppy_auth_token"),
