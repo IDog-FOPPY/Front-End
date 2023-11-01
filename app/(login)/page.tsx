c1e56d779ba78b8be092f29c6a32ff80fe81b5a9
MINJIWON <z_zzz7@fount.co>
2023-11-01 09:35:34 +0900

    password 숨김 처리

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from 'next/navigation';
-import Typo from '@components/core/Typo';
-import logo from '@assets/Logo.png';
-import { login } from '@src/logics/axios';
-import LoginFailedPopup from './LoginFailedPopup';
-import styles from './styles.module.scss';
+import { useRouter } from "next/navigation";
+import Typo from "@components/core/Typo";
+import logo from "@assets/Logo.png";
+import { login } from "@src/logics/axios";
+import LoginFailedPopup from "./LoginFailedPopup";
+import styles from "./styles.module.scss";
 
 export default function LoginPage() {
-
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -17,92 +16,102 @@ export default function LoginPage() {
 
   const [isSplash, setIsSplash] = useState(true);
   useEffect(() => {
-    let timer = setTimeout(() => { setIsSplash(false) }, 3000);
-    return () => { clearTimeout(timer) }
-  }, [isSplash])
-
+    let timer = setTimeout(() => {
+      setIsSplash(false);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isSplash]);
 
   useEffect(() => {
     deleteAllCookies();
     caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        return caches.delete(key);
-      }));
+      return Promise.all(
+        keyList.map(function (key) {
+          return caches.delete(key);
+        })
+      );
     });
-    localStorage.removeItem('foppy_auth_token');
-    localStorage.removeItem('foppy_user_uid');
-
-  }, [])
+    localStorage.removeItem("foppy_auth_token");
+    localStorage.removeItem("foppy_user_uid");
+  }, []);
 
   function deleteAllCookies() {
-    const cookies = typeof window !== 'undefined' ? document.cookie.split(";") : null;
+    const cookies = typeof window !== "undefined" ? document.cookie.split(";") : null;
     if (cookies !== null) {
       for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i];
         const eqPos = cookie.indexOf("=");
         const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        typeof window !== 'undefined' ? document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT" : null;
+        typeof window !== "undefined" ? (document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT") : null;
       }
     }
-
   }
-
-
 
   const onComplete = async () => {
     if (id && pw) {
       const res = await login({
         email: id,
         password: pw,
-      })
-      console.log('res', res);
-      console.log('res.data.token', res.data.accessToken);
+      });
+      console.log("res", res);
+      console.log("res.data.token", res.data.accessToken);
       if (res?.data.accessToken) {
-        typeof window !== 'undefined' ? localStorage.setItem('foppy_auth_token', res.data?.accessToken) : null;
-        typeof window !== 'undefined' ? localStorage.setItem('foppy_user_uid', res.data?.userId) : null;
-        router.push('/main');
+        typeof window !== "undefined" ? localStorage.setItem("foppy_auth_token", res.data?.accessToken) : null;
+        typeof window !== "undefined" ? localStorage.setItem("foppy_user_uid", res.data?.userId) : null;
+        router.push("/main");
       } else {
-        console.log('error!');
+        console.log("error!");
         setIsPopupOpen(true);
       }
-
     }
-  }
-
+  };
 
   return (
     <>
       <div className={styles.pageLayout}>
-        {
-          isSplash ?
-            <div className={styles.splashLogo} style={{ backgroundImage: `url(${logo.src})` }} />
-            :
-            <>
-              <div className={styles.logo} style={{ backgroundImage: `url(${logo.src})` }} />
+        {isSplash ? (
+          <div className={styles.splashLogo} style={{ backgroundImage: `url(${logo.src})` }} />
+        ) : (
+          <>
+            <div className={styles.logo} style={{ backgroundImage: `url(${logo.src})` }} />
 
-              <input type="text" placeholder="아이디" value={id} onChange={(e) => setId(e.target.value)} className={styles.loginBox} />
-              <input type="text" placeholder="비밀번호" value={pw} onChange={(e) => setPw(e.target.value)} className={styles.loginBox} />
-              <Typo color="#606060" variant="caption" style={{ marginLeft: "auto" }}>
-                비회원으로 포마이펫 이용하기
+            <input
+              type="text"
+              placeholder="아이디"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              className={styles.loginBox}
+            />
+            <input
+              type="password"
+              placeholder="비밀번호"
+              value={pw}
+              onChange={(e) => setPw(e.target.value)}
+              className={styles.loginBox}
+            />
+            <Typo color="#606060" variant="caption" style={{ marginLeft: "auto" }}>
+              비회원으로 포마이펫 이용하기
+            </Typo>
+
+            <div className={styles.loginBtn} onClick={onComplete}>
+              <Typo color="white" variant="t3">
+                로그인하기
               </Typo>
+            </div>
 
-              <div className={styles.loginBtn} onClick={onComplete}>
-                <Typo color="white" variant="t3">
-                  로그인하기
-                </Typo>
-              </div>
+            <Typo color="#606060" variant="t3" style={{ marginRight: "auto" }} onClick={() => router.push("/signup")}>
+              회원가입
+            </Typo>
+            <Typo color="#606060" variant="t3" style={{ marginRight: "auto" }}>
+              아이디 찾기 / 비밀번호 찾기
+            </Typo>
 
-              <Typo color="#606060" variant="t3" style={{ marginRight: "auto" }} onClick={() => router.push('/signup')}>
-                회원가입
-              </Typo>
-              <Typo color="#606060" variant="t3" style={{ marginRight: "auto" }}>
-                아이디 찾기 / 비밀번호 찾기
-              </Typo>
-
-              {isPopupOpen && <LoginFailedPopup onClick={() => setIsPopupOpen(false)} />}
-            </>
-        }
+            {isPopupOpen && <LoginFailedPopup onClick={() => setIsPopupOpen(false)} />}
+          </>
+        )}
       </div>
     </>
-  )
+  );
 }
